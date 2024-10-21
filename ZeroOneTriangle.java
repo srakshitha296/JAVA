6b948fcc224209a23c6f035a17256a2f47fbacb3
Rakshitha Shetty <111368698+srakshitha296@users.noreply.github.com>
2024-10-21 10:18:47 +0000

    0 - 1 Triangle

---
+++ ZeroOneTriangle.java
@@ -0,0 +1,28 @@
+/*
+ 1
+ 0 1
+ 1 0 1
+ 0 1 0 1
+ 1 0 1 0 1 
+*/
+import java.util.Scanner;
+class ZeroOneTriangle {
+  public static void main(String[] args){
+    int rows;
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Rows : ");
+    rows = sc.nextInt();
+
+    for(int i=1; i<=rows; i++){
+        for(int j=1; j<=i; j++){
+            if((i+j)%2==0)
+                System.out.print("1 ");
+            else   
+                System.out.print("0 ");
+        }
+        System.out.println();
+    }
+  }  
+
+}
