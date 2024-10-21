b9d0555b133503905c21fa0ea892a9fe8493da2b
Rakshitha Shetty <111368698+srakshitha296@users.noreply.github.com>
2024-10-21 09:48:47 +0000

    Inverted Half Pyramid With Numbers

---
+++ HalfPyramidWithNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HalfPyramidWithNumbers {
+    public static void main(String[] args){
+        int r;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Rows: ");
+        r = sc.nextInt();
+
+        for(int i=1; i<=r; i++){
+            for(int j=1; j<=i ; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        
+
+
+    }
+}
+
