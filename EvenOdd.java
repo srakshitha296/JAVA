8a0efc95f0ba3f8b249c1887c6129c961b40391f
srakshitha296 <srakshitha296@gmail.com>
2024-10-27 13:05:38 +0530

    added the reason

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,4 +1,5 @@
 //Program to Che cck whether a number is even or odd without using division or % operator
+//Bitwise AND operator can be used to check if a number is even. If the least significant bit (LSB) of a number is 0, the number is even.
 public class EvenOdd{
 public static void main(String[] args){
 int n=6;
