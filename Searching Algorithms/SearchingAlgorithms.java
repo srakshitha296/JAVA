711f3863682628b1bfccde5bbdb7498463a1320e
srakshitha296 <srakshitha296@gmail.com>
2024-11-11 08:21:38 +0530

    Binary Search - Recursive approach

--- Searching Algorithms/SearchingAlgorithms.java
+++ Searching Algorithms/SearchingAlgorithms.java
@@ -1,6 +1,6 @@
-public class SearchingAlgorithms {
+class SearchingAlgorithms {
     //Linear Search
-    public static void LinearSearch(int[] array, int key){
+     void LinearSearch(int[] array, int key){
         for(int i=0; i<array.length; i++){
             if(key == array[i]){
                 System.out.println("Element found in index " + i);
@@ -11,10 +11,10 @@ public class SearchingAlgorithms {
     }
 
     //Binary Search -Iterative approach
-    public static void BinarySearchI(int[] array, int key){
+    void BinarySearchI(int[] array, int key){
         int low = 0;
         int high = (array.length)-1;
-        while(low<high){
+        while(low<=high){
             int mid = (low+high)/2;
             if(array[mid]==key){
                 System.out.println("Element found in index " + mid);
@@ -28,14 +28,38 @@ public class SearchingAlgorithms {
         System.out.println("Element not found");
     }
 
+    // Binary Search -Recursive approach
+    void BinarySearchR(int[] array, int low, int high, int key) {
+        if (low > high) {
+            System.out.println("Element not found");
+            return;
+        }
+
+            int mid = (low + high) / 2;
+            if (array[mid] == key) {
+                System.out.println("Element found in index " + mid);
+                return;
+            } else if (key > array[mid])
+                BinarySearchR(array, mid+1, high, key);
+            else
+                BinarySearchR(array, low, mid - 1, key);
+        
+    }
+
     public static void main(String[] args){
         int[] array = {1,2,3,4,5,6,7,8};
+        int key = 4;
+
+        SearchingAlgorithms algo = new SearchingAlgorithms();
 
         System.out.println("LINEAR SEARCH");
-        LinearSearch(array, 9);
+        algo.LinearSearch(array, key);
 
         System.out.println("BINARY SEARCH - Iterative Approach");
-        BinarySearchI(array, 9);
+        algo.BinarySearchI(array, key);
+
+        System.out.println("BINARY SEARCH - Recursive Approach");
+        algo.BinarySearchR(array, 0, array.length-1, key);
         
     }
 }
