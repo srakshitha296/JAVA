15df52d955c701e4eed7360ec0c0ceb7d3baac96
srakshitha296 <srakshitha296@gmail.com>
2024-11-11 08:03:09 +0530

    Binary Search - Iterative approach

--- Searching Algorithms/SearchingAlgorithms.java
+++ Searching Algorithms/SearchingAlgorithms.java
@@ -1,24 +1,41 @@
 public class SearchingAlgorithms {
     //Linear Search
-    public static int LinearSearch(int[] array, int key){
+    public static void LinearSearch(int[] array, int key){
         for(int i=0; i<array.length; i++){
-            if(key == array[i])
-                return i;
+            if(key == array[i]){
+                System.out.println("Element found in index " + i);
+                return;
+            }
         }
-        return -1;
+        System.out.println("Element not found");
     }
 
-
-    
+    //Binary Search -Iterative approach
+    public static void BinarySearchI(int[] array, int key){
+        int low = 0;
+        int high = (array.length)-1;
+        while(low<high){
+            int mid = (low+high)/2;
+            if(array[mid]==key){
+                System.out.println("Element found in index " + mid);
+                return;
+            }
+            else if(key>array[mid])
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+        System.out.println("Element not found");
+    }
 
     public static void main(String[] args){
         int[] array = {1,2,3,4,5,6,7,8};
 
-        System.out.print("LINEAR SEARCH");
-        int index = LinearSearch(array, 4);
-        if(index<0)
-            System.out.print("Element not found");
-        else    
-            System.out.print("Element found in position "+index);
+        System.out.println("LINEAR SEARCH");
+        LinearSearch(array, 9);
+
+        System.out.println("BINARY SEARCH - Iterative Approach");
+        BinarySearchI(array, 9);
+        
     }
 }
